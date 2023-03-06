6d805c9432bcd1bea45fcc1906aad088f61a701b
MartyMasche <115994252+MartyMasche@users.noreply.github.com>
2023-03-06 08:01:32 +0000

    Ho aggiunto una pagina con l'iscrizione

---
+++ style.js
@@ -0,0 +1,29 @@
+// definizione di un array per memorizzare gli iscritti
+var iscritti = [];
+
+// funzione per aggiungere un nuovo iscritto all'array
+function aggiungiIscritto() {
+  // leggi i dati del modulo
+  var nome = document.getElementById("nome").value;
+  var cognome = document.getElementById("cognome").value;
+  var dataNascita = document.getElementById("dataNascita").value;
+  var sport = document.getElementById("sport").value;
+  
+  // aggiungi l'iscritto all'array
+  var nuovoIscritto = {
+    nome: nome,
+    cognome: cognome,
+    dataNascita: dataNascita,
+    sport: sport
+  };
+  iscritti.push(nuovoIscritto);
+  
+  // aggiorna la lista degli iscritti
+  var elenco = document.getElementById("elencoIscritti");
+  var elencoHtml = "";
+  for (var i = 0; i < iscritti.length; i++) {
+    var iscritto = iscritti[i];
+    elencoHtml += "<li>" + iscritto.nome + " " + iscritto.cognome + ", " + iscritto.dataNascita + ", " + iscritto.sport + "</li>";
+  }
+  elenco.innerHTML = elencoHtml;
+}
